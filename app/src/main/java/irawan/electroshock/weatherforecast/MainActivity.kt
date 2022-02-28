21a7c6c3be2a8f2651db58f8eeb4972c0d6228d6
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-02-28 11:30:41 +0100

    SplashScreen added

--- app/src/main/java/irawan/electroshock/weatherforecast/MainActivity.kt
+++ app/src/main/java/irawan/electroshock/weatherforecast/MainActivity.kt
@@ -6,14 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import irawan.electroshock.weatherforecast.navigation.WeatherNavigation
 import irawan.electroshock.weatherforecast.ui.theme.WeatherForecastTheme
@@ -23,7 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherApplication()
+            WeatherApp()
         }
     }
 }
--- app/src/main/java/irawan/electroshock/weatherforecast/screen/WeatherSplashScreen.kt
+++ app/src/main/java/irawan/electroshock/weatherforecast/screen/WeatherSplashScreen.kt
@@ -1,10 +1,20 @@
 package irawan.electroshock.weatherforecast.screen
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import irawan.electroshock.weatherforecast.R
+import irawan.electroshock.weatherforecast.navigation.WeatherScreens
+import irawan.electroshock.weatherforecast.widget.Animation
+import kotlinx.coroutines.delay
 
 @Composable
 fun WeatherSplashScreen(navController: NavController){
-    Text(text = "Splash Screen")
+    LaunchedEffect(key1 = true){
+        delay(925L)
+        navController.popBackStack()
+        navController.navigate(WeatherScreens.MainScreen.name)
+    }
+    Animation(source = LottieCompositionSpec.RawRes(R.raw.logo), size = 200)
 }
